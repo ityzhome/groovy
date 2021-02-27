ca365f203ca97c651f44c0a7451565f7941c0697
Jean-Pol Ypersiel <you@example.com>
2021-02-27 20:54:46 +0100

    C

--- simplegroovy.groovy
+++ simplegroovy.groovy
@@ -8,12 +8,14 @@ pipeline {
 
 
   stages {
-    stage('Stage 1') {
-      withCredentials([azureServicePrincipal('withplugin')]) {
-        echo 'Hello world!'
-        sh 'az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID'
-        sh 'az account set -s $AZURE_SUBSCRIPTION_ID'        
-        }
+    stage('Stage') {
+      steps{
+        withCredentials([azureServicePrincipal('withplugin')]) {
+          echo 'Hello world!'
+          sh 'az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID'
+          sh 'az account set -s $AZURE_SUBSCRIPTION_ID'        
+          }
+         }
     }
 
   }
