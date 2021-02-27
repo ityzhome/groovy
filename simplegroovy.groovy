a0d66b716786438a3325346d58f8f8d62941fd0d
Jean-Pol Ypersiel <you@example.com>
2021-02-27 21:01:46 +0100

    ccc

--- simplegroovy.groovy
+++ simplegroovy.groovy
@@ -10,12 +10,14 @@ pipeline {
   stages {
     stage('Stage') {
       steps{
-        withCredentials([azureServicePrincipal('withplugin')]) {
-          echo 'Hello world!'
-          sh 'az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID'
-          sh 'az account set -s $AZURE_SUBSCRIPTION_ID'        
+          script {
+            withCredentials([azureServicePrincipal('withplugin')]) {
+            
+              sh 'az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID'
+              sh 'az account set -s $AZURE_SUBSCRIPTION_ID'        
           }
          }
+      }
     }
 
   }
